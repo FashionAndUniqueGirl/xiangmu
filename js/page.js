f82e9f7ab7c63b6168470f7c90f174a02d39ee92
wangweiruning <397149747@qq.com>
2017-05-27 18:05:55 +0800

    学院动态
    动态

--- js/page.js
+++ js/page.js
@@ -80,6 +80,7 @@ var  page = {
         });
     },
     "getPageListModel":function(pageCount,currentPage){
+        huidiao()
         var prePage = currentPage-1;
         var nextPage = currentPage+1;
         var prePageClass ="pageItem";
@@ -115,9 +116,46 @@ var  page = {
 
             appendStr+="<li class='"+itemPageClass+"' page-data='"+pageNumber+"' page-rel='itempage'>"+pageNumber+"</li>";
         }
-        appendStr+="<li class='"+nextPageClass+"' page-data='"+nextPage+"' page-rel='nextpage'>&gt;</li>";
+        appendStr+="<li onclick="+'huidiao()'+" class='"+nextPageClass+"' page-data='"+nextPage+"' page-rel='nextpage'>&gt;</li>";
         appendStr+="<li class='gongjiuye' page-data='"+pageCount+"'>共"+showPageNum+"页</li>";
-       return appendStr;
+        var liis = document.getElementsByTagName("li");
+        liis.onclick=function(){
+           return  huidiao();
 
-    }
+        }
+       huidiao();
+       return appendStr;
+       function huidiao(){
+     window._bd_share_config={
+            "common":{
+                "bdSnsKey":{},
+                "bdText":"",
+                "bdMini":"2",
+                "bdPic":"",
+                "bdStyle":"0",
+                "bdSize":"16"},
+                "share":{},
+            "image":{
+                "viewList":[
+                "qzone",
+                "tsina",
+                "tqq",
+                "renren",
+                "weixin"],
+                "viewText":"分享到：",
+                "viewSize":"16"
+            }
+            };
+            with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
+
+        $(".share").mouseenter(function (){
+            $(".share").animate({'left':'0'},300);
+        });
+        $(".share").mouseleave(function(){
+                $(".share").animate({'left':'-230px'},300);
+              });
+
+        } 
+}
+ 
 }
